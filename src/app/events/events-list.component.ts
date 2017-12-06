76f95e455709630615f03890d56272b56a09ac9e
Dennis Xiao <dennisboys@gmail.com>
2017-12-06 21:09:44 +1100

    added toastr service

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
 
 @Component({
   selector: 'events-list',
   templateUrl: 'events-list.component.html',
-  providers: [EventService]
+  providers: [
+    EventService,
+    ToastrService
+  ]
 })
 export class EventsListComponent implements OnInit {
   events: any[];
 
-  constructor(private _eventService: EventService) {
+  constructor(private _eventService: EventService,
+              private _toastrService: ToastrService) {
 
+  }
+
+  handleThumbnailClick(eventName) {
+    this._toastrService.success(eventName);
   }
 
   ngOnInit() {
