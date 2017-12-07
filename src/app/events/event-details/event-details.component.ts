ac6c413baafee9f6587d46ad6f768ba1261743c9
Dennis Xiao <dennisboys@gmail.com>
2017-12-08 10:49:31 +1100

    fixed routing

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: 'event-details.component.html',
@@ -9,12 +10,14 @@ export class EventDetailsComponent implements OnInit {
 
   event: any;
 
-  constructor(private _eventService: EventService) {
+  constructor(private _eventService: EventService,
+              private _route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
-    this.event = this._eventService.getEvent(1);
+    // get the id parameter from URL and convert it into a number
+    this.event = this._eventService.getEvent(parseInt(this._route.snapshot.params['id'], 10));
   }
 
 }
--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -21,7 +21,7 @@ const EVENTS = [
     date: '9/26/2036',
     time: '10:00 am',
     price: 599.99,
-    imageUrl: '/app/assets/images/angularconnect-shield.png',
+    imageUrl: '/assets/images/angularconnect-shield.png',
     location: {
       address: '1057 DT',
       city: 'London',
@@ -99,7 +99,7 @@ const EVENTS = [
     date: '4/15/2037',
     time: '9:00 am',
     price: 950.00,
-    imageUrl: '/app/assets/images/ng-nl.png',
+    imageUrl: '/assets/images/ng-nl.png',
     location: {
       address: 'The NG-NL Convention Center & Scuba Shop',
       city: 'Amsterdam',
@@ -159,7 +159,7 @@ const EVENTS = [
     date: '5/4/2037',
     time: '9:00 am',
     price: 759.00,
-    imageUrl: '/app/assets/images/ng-conf.png',
+    imageUrl: '/assets/images/ng-conf.png',
     location: {
       address: 'The Palatial America Hotel',
       city: 'Salt Lake City',
@@ -241,7 +241,7 @@ const EVENTS = [
     date: '6/10/2037',
     time: '8:00 am',
     price: 800.00,
-    imageUrl: '/app/assets/images/basic-shield.png',
+    imageUrl: '/assets/images/basic-shield.png',
     location: {
       address: 'The UN Angular Center',
       city: 'New York',
@@ -290,7 +290,7 @@ const EVENTS = [
     date: '2/10/2037',
     time: '9:00 am',
     price: 400.00,
-    imageUrl: '/app/assets/images/ng-vegas.png',
+    imageUrl: '/assets/images/ng-vegas.png',
     location: {
       address: 'The Excalibur',
       city: 'Las Vegas',
