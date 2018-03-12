119e9443244fbb3e2c5e134ad90502efb2a566e8
Dennis Xiao <dennisboys@gmail.com>
2018-03-13 10:06:27 +1100

    added a user profile reactive form

--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -1,8 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "../auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  templateUrl: 'user-profile.component.html'
+  templateUrl: 'user-profile.component.html',
+  styleUrls: ['user-profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profileForm: FormGroup;
+  private firstName: FormControl;
+  private lastName: FormControl;
 
+  constructor(private authService: AuthService,
+              private router: Router) {
+
+  }
+
+  saveProfile(formValues) {
+    if (this.profileForm.valid) {
+      // call the authentication service to update user name
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+
+      this.router.navigate(['events']);
+    }
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
+  }
+
+  validateFirstName() {
+    return this.firstName.valid || this.firstName.untouched;
+  }
+
+  validateLastName() {
+    return this.lastName.valid || this.lastName.untouched;
+  }
+
+  ngOnInit() {
+    this.firstName = new FormControl(
+      this.authService.currentUser.firstName,
+      [Validators.required, Validators.pattern('[a-zA-Z].*')]
+    );
+    this.lastName = new FormControl(
+      this.authService.currentUser.lastName,
+      Validators.required
+    );
+
+    this.profileForm = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName
+    });
+  }
 }
