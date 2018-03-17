f9fc3f0a16dffefdfe2d436978bcb254561abf4a
Dennis Xiao <dennisboys@gmail.com>
2018-03-17 18:27:17 +1100

    added session filter feature

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -10,6 +10,7 @@ import { IEvent, ISession } from "../index";
 export class EventDetailsComponent implements OnInit {
   event: IEvent;
   addMode: boolean;
+  filterBy = 'all';
 
   constructor(private eventService: EventService,
               private route: ActivatedRoute) {
--- src/app/events/session-list/session-list.component.ts
+++ src/app/events/session-list/session-list.component.ts
@@ -1,10 +1,30 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { ISession } from "../";
 
 @Component({
   selector: 'session-list',
   templateUrl: 'session-list.component.html'
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[];
+  @Input() filterBy: string;
+  visibleSessions: ISession[];
+
+  ngOnChanges() {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  filterSessions(filter) {
+    // if the filter is set to 'all', just return the whole list
+    // use [...this.sessions] or this.session.slice(0) to clone the session list
+    if (filter === 'all') {
+      this.visibleSessions = [...this.sessions];
+    } else {
+      this.visibleSessions = this.sessions.filter((session) => {
+        return session.level.toLocaleLowerCase() === filter;
+      });
+    }
+  }
 }
